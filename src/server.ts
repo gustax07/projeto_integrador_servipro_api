bbce0c6bacd1ca2f603a756012eb899f0bd5c25d
gustax07 <gfsvieira@gmail.com>
2026-06-03 15:50:23 -0300

    correção do packge,jon e do tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json({ limit: '50kb' }));
 app.use(helmet());
 app.use(globalLimiter);
-
 // ... morgan e outros middlewares
 const stream = {
   write: (message: string) => Logger.http(message.trim()),
